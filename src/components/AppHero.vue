f2f8adb8a8a526b7e3456b9bc61faec28c43571a
SimoneCalzari <simone.calzari@outlook.it>
2023-12-06 16:10:23 +0100

    feat: esercizio base più bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Ciao sono Jumbotron, benvenuto nella mia pagina!",
+      par: "I am a simple hero unit, a simple jumbotron-style component for calling a extra attention to featured content of information",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="jumbo">
+    <div class="container">
+      <h2>{{ title }}</h2>
+      <p>{{ par }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.jumbo {
+  background-color: gainsboro;
+  height: 300px;
+  display: flex;
+}
+
+.container {
+  width: 75%;
+  margin: auto;
+}
+
+h2 {
+  color: white;
+  font-size: 45px;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 30px;
+  color: #1a73e8;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-size: 60px;
+  color: white;
+  background-color: #1a73e8;
+  padding: 20px;
+}
+</style>
